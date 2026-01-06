22e3d5148af9def810e2abe3c6680b1886fac5c4
nathan-sprague <59464678+nathan-sprague@users.noreply.github.com>
2026-01-06 13:31:29 -0500

    Update chat_transformer.py

--- chat_transformer.py
+++ chat_transformer.py
@@ -6,13 +6,17 @@ from tokenizers import ByteLevelBPETokenizer
 
 
 
-model_name = "model_505m_july7_2025.pt"
+model_name = "model_finetuned_no_reason.pt"
 
+question_end_token = 1
+answer_end_token = 2
+think_start_token = 7
+think_end_token = 8
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def generate_text(model, tokenizer, tokens, max_length=100, temperature=0.7, temperature_think=-1, top_p=0.9,
+def generate_text(model, tokenizer, tokens, max_length=100, temperature=0.7, top_p=0.9,
                   repetition_penalty=1.1, stream=False):
     model.clear_kv_cache()
     
@@ -22,18 +26,20 @@ def generate_text(model, tokenizer, tokens, max_length=100, temperature=0.7, tem
     generated = tokens
     # for g in range(generated.shape[1]):
     #     model(generated[:, g:g+1], start_pos=g)
-    model(generated[:, 0:-1], start_pos=0)
-    thinking = True
+    
     last_token = 0
     last_recent_tokens = []
+    new_generated = []
     with torch.no_grad():
-        for _ in range(max_length):
+        # with torch.amp.autocast('cuda', dtype=torch.bfloat16):
+        model(generated[:, 0:-1], start_pos=0)
+
+
+        for tokens_generated in range(max_length):
+            # with torch.amp.autocast('cuda', dtype=torch.bfloat16):
             outputs = model(generated[:, -1:], start_pos=generated.shape[1])
 
-            if temperature_think > 0 and thinking:
-                next_token_logits = outputs[0, :] / temperature
-            else:
-                next_token_logits = outputs[0, :] / temperature
+            next_token_logits = outputs[0, :] / temperature
 
             # Apply repetition penalty
             rp = repetition_penalty
@@ -47,36 +53,33 @@ def generate_text(model, tokenizer, tokens, max_length=100, temperature=0.7, tem
                 else:
                     next_token_logits[0, token_id] *= rp
 
+            # if tokens_generated < 3:
+            #     next_token_logits[0, answer_end_token] -= 100
+            # if tokens_generated == 0:
+            #     next_token_logits[0, generated[0, 0]] -= 100
 
-            # # (Optional) prevent repeating last token explicitly
-            # next_token_logits[0, last_token] -= 10
 
-            # print("gen", generated[:, -1])
-            newline_tokens = [208, 230, 15078, 19]#, 35, 23, 1820]
-            if generated[:, -1].item() in newline_tokens and generated[:, -2].item() in newline_tokens: # bug where it generates new line forever
-                for n in newline_tokens:
-                    next_token_logits[0, n] -= 1000
+            # newline_tokens = [208, 230, 15078, 19]#, 35, 23, 1820]
+            # if generated[:, -1].item() in newline_tokens and generated[:, -2].item() in newline_tokens: # bug where it generates new line forever
+            #     for n in newline_tokens:
+            #         next_token_logits[0, n] -= 1000
                 # print("PREVENTING TOKEN")
 
             # print("Gen->",generated[:, -1].item(), generated[:, -2].item())
-            if thinking:
-                next_token_logits[0, answer_end_token] -= 1000
-                if generated[:, -1].item() == think_end_token:
-                    thinking = False
-            if not thinking:
-                next_token_logits[0, think_end_token] -= 1000
-
-            
-                
+           
 
             next_token_logits = next_token_logits.squeeze()
             filtered_logits = top_p_filtering(next_token_logits, top_p=top_p)
             probabilities = F.softmax(filtered_logits, dim=-1)
 
             next_token = torch.multinomial(probabilities, 1)
+
+
             # if next_token != last_token:
             generated = torch.cat((generated, next_token.unsqueeze(0)), dim=1)
             last_recent_tokens.append(next_token.item())
+
+
             if len(last_recent_tokens)>200:
                 last_recent_tokens = last_recent_tokens[1::]
             # print("next", next_token)
@@ -84,11 +87,13 @@ def generate_text(model, tokenizer, tokens, max_length=100, temperature=0.7, tem
             if last_token == answer_end_token:
                 break
 
+            new_generated.append(next_token.item())
+
             if stream:
                 print(tokenizer.decode([next_token.item()]), end='', flush=True)
 
 
-    return generated[0].tolist()
+    return tokenizer.decode(new_generated)
 
 
 def top_p_filtering(logits, top_p=0.9, filter_value=-float('Inf')):
@@ -106,36 +111,62 @@ def top_p_filtering(logits, top_p=0.9, filter_value=-float('Inf')):
 
 
 
+def chat_template(conversation):
+    tokens = []
+
+    for conv in conversation:
+
+        if conv["message"][0] != "\n":
+            conv["message"] = "\n" + conv["message"]
+        # print(conv)
+        tokens += tokenizer.encode(conv["message"]).ids
+
+        if conv["role"] == "user":
+            tokens += [question_end_token, 208]
+            # print("user", tokens)
+        elif conv["role"] == "assistant":
+            tokens += [answer_end_token]
 
 
-emsize = -1  # embedding dimension
-d_hid = 6144  # dimension of the feedforward network model in ``nn.TransformerEncoder``
-nlayers = 24  # number of ``nn.TransformerEncoderLayer`` in ``nn.TransformerEncoder``
-nhead = 16  # number of heads in ``nn.MultiheadAttention``
-dropout = 0.01  # dropout probability
-ntokens = 50000
-block_size = 4096
-ffn_dim = 4096
-dim = 1024
+
+    return tokens
+
+
+
 
 tokenizer = ByteLevelBPETokenizer.from_file(
     vocab_filename="my_tokenizer_50k_2025/tokenizer_50k_2025-vocab.json",
     merges_filename="my_tokenizer_50k_2025/tokenizer_50k_2025-merges.txt"
 )
+# print(tokenizer.encode('Hello\n\n\n\n').ids)
 
-question_end_token = 1
-answer_end_token = 2
-think_start_token = 7
-think_end_token = 8
+
+
 
 # print(tokenizer.decode([0,1,2,3,4,5,6,7,8]))
 
 
-model = TransformerModel(ntokens, block_size, emsize, nhead, nlayers, ffn_dim=ffn_dim, dim=dim, batch_size=1, device=device).to(device)
+nlayers = 24  # number of ``nn.TransformerEncoderLayer`` in ``nn.TransformerEncoder``
+nhead = 16  # number of heads in ``nn.MultiheadAttention``
+# ntokens = 20000
+dim = 1024+512
+ffn_dim = dim*4
+rope_theta=50000
+ntokens=50000
+max_seq_len=2048
+batch_size=1
+
+# model = TransformerModel(ntokens, max_seq_len, emsize, nhead, nlayers, ffn_dim=ffn_dim, dim=dim, batch_size=batch_size, device=device).to(device)
+model = TransformerModel(ntokens, max_seq_len, -1, nhead, nlayers, ffn_dim=ffn_dim, dim=dim, batch_size=batch_size, device=torch.device("cpu"))
 model.eval()
 
 
-model.load_state_dict(torch.load(model_name, weights_only=True, map_location=device))
+
+model.load_state_dict(torch.load(model_name, weights_only=True, map_location=torch.device("cpu")))
+model.dtype = torch.bfloat16
+if torch.cuda.is_available():
+    model.device = device
+    model = model.to(device)
 
 
 conversation = []
@@ -144,6 +175,7 @@ while True:
     torch.manual_seed(0)
 
     prompt = input("\n>>")
+    # prompt = "What is faster, a turtle or a cheetah?"
 
     if len(prompt) == 0:
        
@@ -153,32 +185,15 @@ while True:
         conversation = []
         continue
 
-    conversation.append(tokenizer.encode(prompt).ids) 
 
-    # print("\nconversation tokens", conversation)
-    tokens = []
-    for i, msg in enumerate(conversation):
-        if i%2==0: # question
-            tokens += msg + [question_end_token]
-        else: # answer
-            tokens += msg + [answer_end_token]
 
-    pre_conv_len = len(tokens)-1
-    tokens.append(think_start_token)
+    conversation.append({"role": "user", "message": prompt})
 
-    # print("\ngiving->", tokenizer.decode(tokens), "\n")
+    # print(chat_template(conversation), tokenizer.decode(chat_template(conversation)))
+    tokens = torch.tensor([chat_template(conversation)], dtype=torch.long).to(device)
 
-    tokens = torch.tensor([tokens], dtype=torch.long).to(device)
-    all_tokens = generate_text(model, tokenizer, tokens, stream=True, temperature=0.6, temperature_think=0.2, top_p=0.9, max_length=4096, repetition_penalty=1.2) 
+    response = generate_text(model, tokenizer, tokens, stream=True, temperature=0.6, top_p=0.9, max_length=2048, repetition_penalty=1.2) 
 
-    if all_tokens[-1] == answer_end_token:
-        all_tokens = all_tokens[0:-1]
 
-    if think_end_token in all_tokens:
-        msg = all_tokens[all_tokens.index(think_end_token)+1::]
-        conversation.append(msg)
-    else:
-        msg = all_tokens[pre_conv_len::]
-        conversation.append(msg)
-
+    conversation.append({"role": "assistant", "message": response})
 
